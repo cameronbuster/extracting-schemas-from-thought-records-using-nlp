95702262f96fd4cd26616a4b0556bf6720390e76
cameronbuster <cbuster.0@gmail.com>
2022-04-07 20:54:51 -0500

    added requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
 def main():
+    coredata_df = pd.read_csv('data/CoreData.csv', sep=';')
+    mentalhealth_df = pd.read_csv('data/MentalHealth.csv', sep=';')
+
+    joined_df = pd.merge(coredata_df, mentalhealth_df, on='Participant.ID', how='left')
+
     return
 
 
